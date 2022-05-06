849cf3cf5d904271c11420b63627903565d73c9a
lalitha <97424403+21A91A05D2@users.noreply.github.com>
2022-05-06 22:09:19 +0530

    n=int(input()) k=n//2+1 for i in range(1,n+1):     for j in range(1,n+1):         if i==j  or j==n-i+1:             print('x',end='')         else:             print( 0,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,9 @@
-a=int(input())
-for i in range(a,0,-1):
-    for j in range(1,i+1):
-        print(j,end='')
-    print('')
+n=int(input())
+k=n//2+1
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==j  or j==n-i+1:
+            print('x',end='')
+        else:
+            print( 0,end='')
+    print()
