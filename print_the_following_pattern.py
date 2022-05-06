8dc93f6741135fe3be32c59024f7674ff19873e6
lalitha <97424403+21A91A05D2@users.noreply.github.com>
2022-05-06 22:12:44 +0530

    n=int(input()) k=n//2+1 for i in range(1,n+1):     for j in range(1,n+1):         if i==j or j==1 or j==n:             print('*',end=' ')         else:             print(' ',end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -2,8 +2,8 @@ n=int(input())
 k=n//2+1
 for i in range(1,n+1):
     for j in range(1,n+1):
-        if i==j :
-            print(0,end='')
+        if i==j or j==1 or j==n:
+            print('*',end=' ')
         else:
-            print( 'x',end='')
+            print(' ',end=' ')
     print()
