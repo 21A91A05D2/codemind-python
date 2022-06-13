206b5cafbf6ea6786723d10f9ebc4fa2981e4a88
lalitha <97424403+21A91A05D2@users.noreply.github.com>
2022-06-13 17:35:52 +0530

    def addDigits(num):     if len(str(num))==1:         return num     while True:         l = [int(i) for i in str(num)]         num = sum(l)         if len(str(num))==1:             return num num=int(input()) print(addDigits(num))

---
+++ Add_Digits.py
@@ -0,0 +1,10 @@
+def addDigits(num):
+    if len(str(num))==1:
+        return num
+    while True:
+        l = [int(i) for i in str(num)]
+        num = sum(l)
+        if len(str(num))==1:
+            return num
+num=int(input())
+print(addDigits(num))
