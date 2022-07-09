1aab7f854ad05d1aedd0c6252f74ea27b78403bb
lalitha <97424403+Lalitha307@users.noreply.github.com>
2022-07-09 11:06:39 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 v=0 for i in range(0,len(a)-1):     if a[i]<a[i+1]:         c=0     else:         v=-1
    if v==-1:
        print('no')
    else:
        print('yes')

---
+++ Ascending_array.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+v=0
+for i in range(0,len(a)-1):
+    if a[i]<a[i+1]:
+        c=0
+    else:
+        v=-1
+    
+
+if v==-1:
+    print('no')
+else:
+    print('yes')
