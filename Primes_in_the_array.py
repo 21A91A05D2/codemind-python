fe15108c06a2871136236d212eab60b0d8d56c74
lalitha <97424403+Lalitha307@users.noreply.github.com>
2022-07-11 05:50:14 +0530

    def prime(n):     if n==1:         return 0     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return 0         return 1
    n = int(input())
    arr = list(map(int,input().split()))
    
    count = 0
    
    for i in range(0,len(arr)):
        if prime(arr[i]):
            count+=1
    print(count)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,18 @@
+def prime(n):
+    if n==1:
+        return 0
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return 0
+        return 1
+
+n = int(input())
+arr = list(map(int,input().split()))
+
+count = 0
+
+for i in range(0,len(arr)):
+    if prime(arr[i]):
+        count+=1
+print(count)
