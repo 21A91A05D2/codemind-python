34d7a2ea85bc03ea990edb61dfc0b60dad2df699
lalitha <97424403+Lalitha307@users.noreply.github.com>
2022-10-07 18:37:19 +0530

    N=int(input()) fcount=0 for i in range(1,N+1):     if N%i==0:         count=0         for j in range(1,i+1):             if i%j==0:                 count+=1         if count!=2:             fcount+=1 print(fcount)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,11 +1,11 @@
-c=0
-n=int(input())
-for i in range(1,n+1):
-    if(n%i==0):
-        fc=0
+N=int(input())
+fcount=0
+for i in range(1,N+1):
+    if N%i==0:
+        count=0
         for j in range(1,i+1):
-            if(i%j==0):
-                fc+=1
-        if(fc!=2):
-            c+=1
-print(c)
+            if i%j==0:
+                count+=1
+        if count!=2:
+            fcount+=1
+print(fcount)
